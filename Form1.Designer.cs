3799fe2d0b4c0f9464587833d5c1ddd1a41d4628
Dmytro Karpovuch <ZAYEC77@gmail.com>
2013-09-12 15:14:33 +0300

    Second commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,6 +33,12 @@
             this.label1 = new System.Windows.Forms.Label();
             this.output = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // btn
@@ -43,12 +49,13 @@
             this.btn.TabIndex = 0;
             this.btn.Text = "Сodify";
             this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
             // 
             // input
             // 
             this.input.Location = new System.Drawing.Point(16, 29);
             this.input.Name = "input";
-            this.input.Size = new System.Drawing.Size(257, 20);
+            this.input.Size = new System.Drawing.Size(458, 20);
             this.input.TabIndex = 1;
             // 
             // label1
@@ -62,32 +69,87 @@
             // 
             // output
             // 
-            this.output.Location = new System.Drawing.Point(16, 101);
+            this.output.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.output.Location = new System.Drawing.Point(16, 229);
             this.output.Name = "output";
-            this.output.Size = new System.Drawing.Size(257, 20);
+            this.output.Size = new System.Drawing.Size(458, 20);
             this.output.TabIndex = 1;
             // 
             // label2
             // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 85);
+            this.label2.Location = new System.Drawing.Point(13, 213);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(39, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Output";
             // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(172, 60);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(52, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Cesar";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(172, 83);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(48, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.Text = "Date";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(230, 79);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(230, 57);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(53, 20);
+            this.numericUpDown1.TabIndex = 6;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(172, 106);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(59, 17);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Square";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(285, 261);
+            this.ClientSize = new System.Drawing.Size(486, 261);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.output);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.input);
             this.Controls.Add(this.btn);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Crypt";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -100,6 +162,11 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox output;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.RadioButton radioButton3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,5 +15,40 @@ namespace ITK
         {
             InitializeComponent();
         }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+                output.Text = Cesar(input.Text, 12);
+            else
+                if (radioButton2.Checked)
+                    output.Text = Date(input.Text, dateTimePicker1.Value);
+                else
+                    output.Text = Square(input.Text);
+        }
+
+        protected string Square(String input)
+        {
+            string s = "";
+            int length = Convert.ToInt32(Math.Floor(Math.Sqrt(input.Length+1)));
+            MessageBox.Show(length.ToString());
+            return s;
+        }
+
+        protected string Date(String input, DateTime date)
+        {
+            int offset = date.Year + date.Month + date.Day;
+            return Cesar(input, offset);
+        }
+
+        protected string Cesar(string input, int offset)
+        {
+            String s = "";
+            foreach (char c in input)
+            {
+                s += (char)(c + offset);
+            }
+            return s;
+        }
     }
 }
